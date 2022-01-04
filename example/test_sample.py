3690d926ef797b709599280cc68cf0ea881ae9f4
Sabi Maharjan <smaharjan@outcodesoftware.com>
2022-01-04 13:50:03 +0545

    Added new testcase

--- example/test_sample.py
+++ example/test_sample.py
@@ -2,13 +2,7 @@ from django.test import TestCase
 import unittest
 
 
-class DummyTest(TestCase):
-
-    def test_dummy(self):
-        self.assertEqual(True, True)
-
-
 class TestStringMethods(unittest.TestCase):
-z
+
         def test_upper(self):
             self.assertEqual('foo'.upper(), 'FOO')
